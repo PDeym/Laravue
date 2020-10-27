563a946acd739b7d76ca131931d024c4a5fcfb01
PAZ_JANPAULO <jpaulopaz08@gmail.com>
2020-10-27 08:22:39 +0800

    First Commit

---
+++ resources/views/job_posting/index.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Job Listing</title>
+
+  <!-- Custom fonts for this theme -->
+  <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+  <!-- Theme CSS -->
+  <link href="{{ asset('css/freelancer.min.css') }}" rel="stylesheet">
+
+</head>
+<body>
+    <div id="app">
+        <br/>
+        <div class="container">
+      
+            <!-- Portfolio Section Heading -->
+            <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">JOB POSTING (VUE CRUD)</h2>
+              <br/>
+            <job-posting-component :posting="{{$posting}}"/>
+    
+          </div>
+    </div>
+</body>
+<!-- Bootstrap core JavaScript -->
+<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+<!-- Plugin JavaScript -->
+<script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+<!-- Contact Form JavaScript -->
+<script src="{{ asset('js/jqBootstrapValidation.js') }}"></script>
+<script src="{{ asset('js/contact_me.js') }}"></script>
+
+<!-- Custom scripts for this template -->
+<script src="{{ asset('js/freelancer.min.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+</html>
